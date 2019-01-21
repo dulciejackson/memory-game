6175617b7a8de3d0d0d72c8670dd49ccf4a0f3c3
Fabio Oesch <fabiooesch@hotmail.com>
2019-01-21 17:14:45 +0000

    The progress bar looks horrendous but it's a thing

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
-$(function(){
-	$('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
-		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
-		$(this).addClass('selected');
-		$(this).find('input[type="radio"]').prop("checked", true);
+$(function () {
+    $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function () {
+        $(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
+        $(this).addClass('selected');
+        $(this).find('input[type="radio"]').prop("checked", true);
 
-	});
+    });
 });
 
-$(function() {
-	$('#closing').on("click", function () {
-	    $(this).parent().hide();
-	    $(this).parent().parent().find('#' + $(this).parent().attr('class')).show();
-	});
+// Goes from the landing page to the first memory game
+$(function () {
+    $('#landing-page-btn').on("click", function () {
+        $(this).parent().hide();
+        let parent = $(this).parent().parent().find('#game');
+        $(parent).show();
+        $(parent).find('#progress').show();
+        $(parent).find('#memory').show();
+        $(parent).find('#remember').hide();
+        alert("is this done?");
+        $('.progress-bar-fill').css('width', '100%')
+    });
 });
 
-function slide_div(toHide, toAppear) {
-
-}
