e9f7ed520cc6095f75cf0e0eb321c485b1bb9393
zdac036 <zdac036@live.rhul.ac.uk>
2019-01-22 21:15:26 +0000

    altered line to match its use in the game

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var completes = document.querySelectorAll(".complete");
+
+$(function () {
+	$('#toggleButton').on("click", function () {
+		$('#toggleButton').css("background-color", "#e66767")
+	})
+})
