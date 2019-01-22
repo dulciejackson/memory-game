c4ac88f8ceac9d117bc9f61b5e27d19440563b67
Fabio Oesch <fabiooesch@hotmail.com>
2019-01-22 15:24:00 +0000

    just reformated everything with 2 spaces

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // Highlights selected item type (images, numbers or words)
 $(function () {
-    $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function () {
-        $(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
-        $(this).addClass('selected');
-        $(this).find('input[type="radio"]').prop("checked", true);
+  $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function () {
+    $(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
+    $(this).addClass('selected');
+    $(this).find('input[type="radio"]').prop("checked", true);
 
-    });
+  });
 });
 
 // Return to landing page when home button clicked
@@ -20,14 +20,14 @@ $(function () {
 
 // Goes from the landing page to the first memory game
 $(function () {
-    $('#landing-page-btn').on("click", function () {
-        $(this).parent().hide();
-        let parent = $(this).parent().parent().find('#game');
-        $(parent).show();
-        $(parent).find('#progress').show();
-        $(parent).find('#memory').show();
-        $(parent).find('#remember').hide();
-        alert("is this done?");
-        $('.progress-bar-fill').css('width', '100%')
-    });
+  $('#landing-page-btn').on("click", function () {
+    $(this).parent().hide();
+    let parent = $(this).parent().parent().find('#game');
+    $(parent).show();
+    $(parent).find('#progress').show();
+    $(parent).find('#memory').show();
+    $(parent).find('#remember').hide();
+    alert("is this done?");
+    $('.progress-bar-fill').css('width', '100%')
+  });
 });
