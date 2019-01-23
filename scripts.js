f11c1f85396ae4a467ca5ceeaf8abc2666df1b6a
zdac036 <zdac036@live.rhul.ac.uk>
2019-01-23 02:02:51 +0000

    basic clickable timeline

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
-var completes = document.querySelectorAll(".complete");
-
 $(function () {
-	$('#toggleButton').on("click", function () {
-		$('#toggleButton').css("background-color", "#e66767")
-	})
+  $('#button1').on("click", function () {
+    $('#stage1').toggleClass("complete");
+  })
+  
+  $('#button2').on("click", function () {
+    $('#stage2').toggleClass("complete") 
+  })
+  
+  $('#button3').on("click", function () {
+    $('#stage3').toggleClass("complete") 
+  })
+  
+  $('#button4').on("click", function () {
+    $('#stage4').toggleClass("complete") 
+  }) 
+  
+  $('#button5').on("click", function () {
+    $('#stage5').toggleClass("complete") 
+  })
+  
+  $('#button6').on("click", function () {
+    $('#stage6').toggleClass("complete") 
+  })
+  
 })
