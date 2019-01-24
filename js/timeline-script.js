c08b7b1cf1e9cd4fec383215e18d1bff14f81d0c
n-neumann <noamjneumann@gmail.com>
2019-01-24 12:42:25 -0200

    Aligned progress-bar and functionality of one-select

--- js/timeline-script.js
+++ js/timeline-script.js
@@ -1,27 +1,48 @@
+function show(stage){
+  $(stage).addClass("complete");
+}
+
+function hideExcept(stage){
+  var screens = ["stage1", "stage2", "stage3", "stage4", "stage5", "stage6"];
+  
+  screens = jQuery.grep(screens, function(value){
+    return value != stage;
+  });
+
+  for(var i=0; i<screens.length; i++){
+    $("#"+screens[i]).removeClass("complete");
+  }
+}
+
 $(function () {
 	$('#stage1').on("click", function () {
-    $(this).toggleClass("complete");
+    console.log(this);
+    hideExcept("stage1");
+    show("#stage1");
 	})
   
   $('#stage2').on("click", function () {
-// Used to toggle all 'li' DOMs
-//    $('li').toggleClass("complete")
-    $(this).toggleClass("complete");
+    hideExcept("stage2");
+    show("#stage2");
 	})
   
   $('#stage3').on("click", function () {
-    $(this).toggleClass("complete");
+    hideExcept("stage3");
+    show("#stage3");
 	})
   
   $('#stage4').on("click", function () {
-    $(this).toggleClass("complete");
+    hideExcept("stage4");
+    show("#stage4");
 	}) 
   
   $('#stage5').on("click", function () {
-    $(this).toggleClass("complete");
+    hideExcept("stage5");
+    show("#stage5");;
 	})
   
   $('#stage6').on("click", function () {
-    $(this).toggleClass("complete");
+    hideExcept("stage6");
+    show("#stage6");
 	})
 })
