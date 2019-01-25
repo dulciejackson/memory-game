4f64ed05b4bfdd1ccf4f1c1a62388eca18528689
Fabio Oesch <fabiooesch@hotmail.com>
2019-01-25 00:34:16 +0000

    more stuff has changed

--- js/report-chart.js
+++ js/report-chart.js
@@ -3,32 +3,37 @@ let totalResults = 0;
 var ctx = document.getElementById("myChart").getContext('2d');
 
 function create_chart() {
+  var backgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)'
+  ];
+  var borderColor = [
+    'rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+  ];
   var chart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"], // iterate array of SCREENS once available
+      labels: ["Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6"].slice(0, aTotal.length - 1), // iterate array of SCREENS once available
       datasets: [{
         data: aTotal, // retrieve data once user has completed
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
+        backgroundColor: backgroundColor.slice(0, aTotal.length - 1),
+        borderColor: borderColor.slice(0, aTotal.length - 1),
         borderWidth: 1
       }]
     },
     options: {
+      title: {
+        display: false
+      },
       responsive: true,
       maintainAspectRatio: false,
       scales: {
@@ -47,9 +52,10 @@ function calculate_result() {
   let aResults = results.split(",");
   for (let i = 0; i < aGuesses.length; i++) {
     totalResults += aGuesses[i].length;
-    aTotal[i] = get_common_elems(aResults[i], aGuesses[i]) / aGuesses[i].length;
+    aTotal[i] = get_common_elems(aResults[i], aGuesses[i]) / aResults[i].length;
   }
   create_chart();
+  show_total();
 }
 
 function show_total() {
